21f01efe82c2820e08f85df2542e6d64cd9c2de2
MOTYSHIZ <MOTYSHIZ@gmail.com>
2019-05-27 13:50:17 -0700

    Initial Commit. Don't Forget that VRTK v4 Beta is a submodule, and may need to be reimported on its own.

---
+++ Assets/Scripts/SceneConsoleTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneConsoleTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Scene Loaded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    public void printTestString()
+    {
+        Debug.Log("Test String");
+    }
+}
